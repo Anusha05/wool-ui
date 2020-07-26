953ad8c90d4e59bb4b1414e974dd8cac3a4698e2
Anusha Bandekar <anubandekar5@gmail.com>
2020-07-26 15:47:02 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//
+//ReactDOM.render(
+//  React.createElement('h1', null, 'Hello from React!'),
+//  document.getElementById('root')
+//);
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import Volunteer from "./components/volunteer";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
-  React.createElement('h1', null, 'Hello from React!'),
-  document.getElementById('root')
+  <React.StrictMode>
+    <Volunteer />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
