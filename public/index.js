1df5de0083d1d60bba9f0bc46d119593a645a67b
Anusha Bandekar <anubandekar5@gmail.com>
2020-07-26 16:33:19 -0700

    making index.js public

---
+++ public/index.js
@@ -0,0 +1,18 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//
+//ReactDOM.render(
+//  React.createElement('h1', null, 'Hello from React!'),
+//  document.getElementById('root')
+//);
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello from React!</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
