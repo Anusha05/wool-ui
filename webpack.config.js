45c75ae90487fdba8571318120813842385c82e3
Anusha Bandekar <anubandekar5@gmail.com>
2020-07-26 13:43:53 -0700

    Updated contentBase.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         chunkFilename: '[name].html'
     },
     devServer: {
-        contentBase: __dirname + "/public/",
+        contentBase: __dirname + "/public/index.html",
         inline: true,
         host: '0.0.0.0',
         port: 8080,
