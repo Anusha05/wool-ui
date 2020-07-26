c455f6a115482451bacb5c19abc8900576019a43
Anusha Bandekar <anubandekar5@gmail.com>
2020-07-26 16:06:53 -0700

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        },
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          query: {
+           presets: ['es2015', 'react']
+          }
         }
       },
       {
