7b81145cba1515868c089692c106751ab5aefdfc
Anusha Bandekar <anubandekar5@gmail.com>
2020-07-26 14:58:37 -0700

    Update index.js.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,9 @@ import Volunteer from "./components/volunteer";
 import "bootstrap/dist/css/bootstrap.css";
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Volunteer />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Index />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
