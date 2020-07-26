2881f3c1a73ddfbd0a4213df5d1de3bdc5c30523
Anusha Bandekar <anubandekar5@gmail.com>
2020-07-26 13:22:50 -0700

    Adding webpack.config.js file.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    entry: __dirname + "/src/index.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "index.html",
+        chunkFilename: '[name].html'
+    },
+    devServer: {
+        contentBase: __dirname + "/public/",
+        inline: true,
+        host: '0.0.0.0',
+        port: 8080,
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(jpg|jpeg|gif|png|ico)$/,
+                exclude: /node_modules/,
+                loader: 'file-loader?name=[name].[ext]'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ["es2016", "react", "env", "stage-2"]
+                }
+            }
+        ]
+    }
+};
